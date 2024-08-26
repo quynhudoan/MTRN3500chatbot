7ca30bc089735382bb2057ea27e07d8f318bec78
quynh doan <quynhd2001@gmail.com>
2024-08-26 18:01:24 +1000

    adding files

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,17 @@ import time
 
 client = OpenAI(api_key=st.secrets["API_key"])
 
+vector_store = client.beta.vector_stores.create(name="Chatbot Training")
+
+file_paths = ["files/CO.pdf", "files/Jays-Notes.pdf"]
+file_streams = [open(path, "rb") for path in file_paths]
+ 
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+batch = client.beta.vector_stores.file_batches.upload_and_poll(
+    vector_store_id=vector_store.id, files=file_streams
+)
+
 def app():
     st.title("MTRN3500 Study Buddy")
 
@@ -16,7 +27,7 @@ def app():
             instructions="This GPT, named 'MTRN3500 Study Buddy,' will answer course-specific questions about the undergraduate UNSW course MTRN3500. It will not answer any questions outside of this course or related to mechatronics. The tone of the responses will always be friendly, kind, supportive, and welcoming, while also being engaging and encouraging. It will help students feel confident and motivated in their learning journey. The responses will be detailed, yet easy to understand, guiding students through the complexities of the course material with patience, clarity, and always with a friendly, reassuring approach. All responses should be engaging, kind, friendly and very welcoming, focusing on supporting the student's wellbeing. The GPT will ensure its responses are as accurate as possible to give precise and reliable information related to the course.",
             model="gpt-4o",
             tools=[{"type": "file_search"}],
-            tool_resources={"file_search": {"vector_stores": {"file_ids": ["file-2GeQaiOZU3xQdmbOACG0JX1v", "file-E47aeY3Cr9D9Ul9FGhRTbDl1"]}}},
+            tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
         )
 
         # create a Thread
