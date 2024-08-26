142a1e360a55e256e51e70afe65a4b7c617f7eef
quynh doan <quynhd2001@gmail.com>
2024-08-26 18:59:32 +1000

    adjust image

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ client = OpenAI(api_key=st.secrets["API_key"])
 
 def app():
     img = Image.open("logo.png")
-    new_size = (75, 150)
+    new_size = (150, 75)
     img = img.resize(new_size)
     st.image(img)
 
