e7237ebf3514ebac39cf82f2e5d0f84683a12ce9
quynh doan <quynhd2001@gmail.com>
2024-09-03 18:02:25 +1000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -27,11 +27,11 @@ def authenticate_gdrive():
 
     client_config = {
         "installed": {
-            "client_id": str(client_id),
-            "client_secret": str(client_secret),
-            "redirect_uris": [str(redirect_uris)],
-            "auth_uri": str(auth_uri),
-            "token_uri": str(token_uri)
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "redirect_uris": ["http://localhost"],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token"
         }
     }
 
